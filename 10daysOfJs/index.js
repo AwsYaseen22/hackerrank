a30121191d1338e18d64100ff089453efb163e45
AwsYaseen22 <aws.yaseen.22@gmail.com>
2023-03-28 23:50:49 +0300

    Day 2: Conditional Statements: Switch

--- 10daysOfJs/index.js
+++ 10daysOfJs/index.js
@@ -127,5 +127,27 @@ function getGrade ( score ) {
 
 // ##########################################
 
+// Day 2: Conditional Statements: Switch
+
+function getLetter ( s ) {
+    let letter;
+    // Write your code here
+    switch ( true ) {
+        case 'aeiou'.includes( s[0] ):
+            letter = 'A'
+            break
+        case 'bcdfg'.includes( s[0] ):
+            letter = 'B'
+            break
+        case 'hjklm'.includes( s[0] ):
+            letter = 'C'
+            break
+        default:
+            letter = 'D'
+            break
+    }
+    return letter;
+}
+
 
 // ##########################################
