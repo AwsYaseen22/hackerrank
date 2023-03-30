b72b52723d3c4a41a5a56e0620252aa6cb6652a7
AwsYaseen22 <aws.yaseen.22@gmail.com>
2023-03-30 23:20:16 +0300

    Day 4: Create a Rectangle Object

--- 10daysOfJs/index.js
+++ 10daysOfJs/index.js
@@ -222,3 +222,17 @@ function isPositive ( a ) {
 
 
 // ##########################################
+
+// Day 4: Create a Rectangle Object
+
+function Rectangle ( a, b ) {
+    return {
+        length: a,
+        width: b,
+        perimeter: 2 * a + 2 * b,
+        area: a * b
+    }
+}
+
+
+// ##########################################
