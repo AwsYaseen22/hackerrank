fcfa1bc785543139e1e0bcaf8760f32c562af3d9
AwsYaseen22 <aws.yaseen.22@gmail.com>
2023-04-02 23:21:10 +0300

    Day 8: Create a Button

--- 10daysOfJs/index.js
+++ 10daysOfJs/index.js
@@ -380,3 +380,43 @@ function regexVar () {
 
 
 // ##########################################
+
+//  Day 8: Create a Button
+
+/*
+// CSS
+#btn{
+    width: 96px;
+    height: 48px;
+    font-size: 24px;
+    cursor: pointer;
+}
+
+// JS
+  let btn = document.getElementById('btn');
+  let counter = 0;
+  btn.innerHTML = counter;
+  btn.onclick = ()=> {
+      counter++
+      btn.innerHTML = counter
+  };
+
+// html
+<!-- Enter your HTML code here -->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Button</title>
+        <link rel="stylesheet" href="css/button.css" type="text/css">
+    </head>
+    <body>
+        <button id="btn"></button>
+        <script src="js/button.js" type="text/javascript"></script>
+    </body>
+</html>
+
+
+*/
+
+// ##########################################
