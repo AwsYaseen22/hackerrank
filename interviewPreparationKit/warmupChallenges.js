e5ba5fc6823f551309bf6418a61fd9d9e30d5c8c
AwsYaseen22 <aws.yaseen.22@gmail.com>
2023-04-15 23:34:04 +0300

    solve warmup challenge

--- interviewPreparationKit/warmupChallenges.js
+++ interviewPreparationKit/warmupChallenges.js
@@ -22,3 +22,27 @@ function jumpingOnClouds ( c ) {
 // console.log( jumpingOnClouds( [0, 0, 0, 0, 1, 0] ) );
 
 // ######################################################
+
+// Repeated String
+
+function repeatedString ( s, n ) {
+    let aCounter = ( str ) => str.length ? str.split( '' ).filter( l => l === 'a' ).length : 0
+
+    let len = n / s.length
+    let remain = n % s.length
+    let string = s.slice( 0, Math.floor( remain ) )
+
+    let stringA = aCounter( string )
+    if ( s.length > n ) {
+        return stringA
+    } else {
+        return stringA + ( aCounter( s ) * Math.floor( len ) )
+    }
+}
+
+// console.log( repeatedString( 'abcac', 10 ) );
+// console.log( repeatedString( 'aba', 10 ) );
+// console.log( repeatedString( 'a', 1000000000000 ) );
+
+
+// ######################################################
